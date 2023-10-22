104ce77a1390bf803fe537c7d781b9408d9c068f
KidBourbon <kidbourbongamer@gmail.com>
2023-10-22 17:48:02 -0400

    Implemented the functionality to get all categories.

--- src/lib/main.js
+++ src/lib/main.js
@@ -1,41 +1,45 @@
-const apiUrlV3 = 'https://api.themoviedb.org/3';
+// import axios from 'axios';
 
-const moviePath = '/movie';
-const trendingMoviesPath = '/trending/movie';
+const apiV3 = axios.create({
+	baseURL: 'https://api.themoviedb.org/3',
+	headers: {
+		accept: 'application/json',
+		Authorization: `Bearer ${ACCESS_TOKEN}`,
+	},
+});
+
+const posterPathWidth300 = 'https://image.tmdb.org/t/p/w300';
+const trendingMoviesPath = '/trending/movie/day';
 const categoryPath = '/genre/movie/list';
 
 const trendingPreviewMovieList = document.getElementById(
 	'trending-preview-movie-list'
 );
 
-async function fetchData(urlApi, options) {
-	const response = await fetch(urlApi, options);
-	const data = response.json();
-	return data;
-}
+const categoriesPreviewList = document.getElementById(
+	'categories-preview-list'
+);
 
+/**
+ * Gets trending movies.
+ */
 async function getTrendingMoviesPreview() {
-	const response = await fetchData(
-		apiUrlV3 + trendingMoviesPath + '/day?language=en-US',
-		{
-			method: 'GET',
-			headers: {
-				accept: 'application/json',
-				Authorization: `Bearer ${ACCESS_TOKEN}`,
-			},
-		}
-	);
+	const { data } = await apiV3(trendingMoviesPath);
 
-	const movies = response.results;
+	const movies = data.results;
 	generateTrendingMoviesPreview(movies);
 }
 
+/**
+ * Generates movie cards for each movie in the list.
+ * @param {Array} movies List of movies.
+ */
 function generateTrendingMoviesPreview(movies) {
 	movies.forEach((movie) => {
 		trendingPreviewMovieList.innerHTML += `
       <div class="movie">
         <img
-          src="https://image.tmdb.org/t/p/w300${movie.poster_path}"
+          src="${posterPathWidth300}${movie.poster_path}"
           class="movie__img"
           alt="${movie.title}"
         />
@@ -44,4 +48,30 @@ function generateTrendingMoviesPreview(movies) {
 	});
 }
 
+/**
+ * Gets all categories.
+ */
+async function getCategoriesPreview() {
+	const { data } = await apiV3(categoryPath);
+
+	const categories = data.genres;
+	generateCategoriesPreview(categories);
+}
+
+/**
+ * Generates a div for each category in the list.
+ * @param {Array} categories List of categories.
+ */
+function generateCategoriesPreview(categories) {
+	categories.forEach((category) => {
+		categoriesPreviewList.innerHTML += `
+      <div class="category">
+				<h3 id="id${category.id}" class="category__title">${category.name}</h3>
+      </div>
+    `;
+	});
+}
+
+// Start app
 getTrendingMoviesPreview();
+getCategoriesPreview();
