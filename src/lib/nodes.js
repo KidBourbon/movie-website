a26c0e12bc4b4a11d1603e314686c3afa475ba60
KidBourbon <kidbourbongamer@gmail.com>
2023-10-23 05:11:40 -0400

    Listed all nodes

---
+++ src/lib/nodes.js
@@ -0,0 +1,52 @@
+/*
+ * *************
+ *   SECTIONS
+ * *************
+ */
+
+//#region Header
+const headerSection = document.querySelector('#header');
+const headerArrow = document.querySelector('.header__arrow');
+const headerTitle = document.querySelector('.header__title');
+const headerTitleCategoryView = document.querySelector(
+	'.header__title--category-view'
+);
+const searchForm = document.querySelector('#search-form');
+const searchBtn = document.querySelector('#search-btn');
+//#endregion
+
+//#region Trending Preview
+const trendingPreviewSection = document.querySelector('#trending-preview');
+const trendingPreviewHeader = document.querySelector(
+	'.trending-preview__header'
+);
+const trendingPreviewTitle = document.querySelector('.trending-preview__title');
+const trendingPreviewBtn = document.querySelector('.trending-preview__btn');
+const trendingPreviewMovieList = document.querySelector(
+	'#trending-preview-movie-list'
+);
+//#endregion
+
+//#region Categories Preview
+const categoriesPreviewSection = document.querySelector('#categories-preview');
+const categoriesPreviewTitle = document.querySelector(
+	'.categories-preview__title'
+);
+const categoriesPreviewList = document.querySelector(
+	'#categories-preview-list'
+);
+//#endregion
+
+//#region Generic List
+const genericListSection = document.querySelector('#generic-list');
+//#endregion
+
+//#region Movie Detail
+const movieDetailSection = document.querySelector('#movie-detail');
+const movieDetailTitle = document.querySelector('.movie-detail__title');
+const movieDetailScore = document.querySelector('.movie-detail__score');
+const movieDetailDescription = document.querySelector(
+	'.movie-detail__description'
+);
+const categoriesList = document.querySelector('.categories-list');
+//#endregion
