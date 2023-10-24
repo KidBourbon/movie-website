43177b70f48184dde1a652ddb97316203dc87074
KidBourbon <kidbourbongamer@gmail.com>
2023-10-24 01:17:39 -0400

    Changed the querySelector method to getElementById

--- src/lib/nodes.js
+++ src/lib/nodes.js
@@ -5,44 +5,44 @@
  */
 
 //#region Header
-const headerSection = document.querySelector('#header');
+const headerSection = document.getElementById('header');
 const headerArrow = document.querySelector('.header__arrow');
 const headerTitle = document.querySelector('.header__title');
 const headerTitleCategoryView = document.querySelector(
 	'.header__title--category-view'
 );
-const searchForm = document.querySelector('#search-form');
-const searchBtn = document.querySelector('#search-btn');
+const searchForm = document.getElementById('search-form');
+const searchBtn = document.getElementById('search-btn');
 //#endregion
 
 //#region Trending Preview
-const trendingPreviewSection = document.querySelector('#trending-preview');
+const trendingPreviewSection = document.getElementById('trending-preview');
 const trendingPreviewHeader = document.querySelector(
 	'.trending-preview__header'
 );
 const trendingPreviewTitle = document.querySelector('.trending-preview__title');
 const trendingPreviewBtn = document.querySelector('.trending-preview__btn');
-const trendingPreviewMovieList = document.querySelector(
-	'#trending-preview-movie-list'
+const trendingPreviewMovieList = document.getElementById(
+	'trending-preview-movie-list'
 );
 //#endregion
 
 //#region Categories Preview
-const categoriesPreviewSection = document.querySelector('#categories-preview');
+const categoriesPreviewSection = document.getElementById('categories-preview');
 const categoriesPreviewTitle = document.querySelector(
 	'.categories-preview__title'
 );
-const categoriesPreviewList = document.querySelector(
-	'#categories-preview-list'
+const categoriesPreviewList = document.getElementById(
+	'categories-preview-list'
 );
 //#endregion
 
 //#region Generic List
-const genericListSection = document.querySelector('#generic-list');
+const genericListSection = document.getElementById('generic-list');
 //#endregion
 
 //#region Movie Detail
-const movieDetailSection = document.querySelector('#movie-detail');
+const movieDetailSection = document.getElementById('movie-detail');
 const movieDetailTitle = document.querySelector('.movie-detail__title');
 const movieDetailScore = document.querySelector('.movie-detail__score');
 const movieDetailDescription = document.querySelector(
